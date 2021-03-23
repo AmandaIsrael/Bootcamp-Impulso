5ca1a67a705505cebc9c0d1c19f581206dd5e161
AmandaIsrael <amanda_israel@hotmail.com>
2021-03-23 19:29:24 -0300

    Snake Movement

--- Snake/JS/Script.js
+++ Snake/JS/Script.js
@@ -1,14 +1,18 @@
 let canvas = document.getElementById("Snake");
 let context = canvas.getContext("2d");
-let box = 32;
+let box = 32, jogo;
+let direction = "right";
+
 let snake = [];
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
 
-function criarBG(){
+function createBackGround(){
     context.fillStyle = 'green';
+
+    // x, y, widht, height 
     context.fillRect(0, 0, 16 * box, 16 * box);
     context.strokeRect(15, 15, 15 * box, 15 * box);
 }
@@ -20,5 +24,27 @@ function createSnake(){
     }
 }
 
-criarBG();
-createSnake();
+function startGame(){
+    
+    createBackGround();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+jogo = setInterval(startGame, 500); //100ms
